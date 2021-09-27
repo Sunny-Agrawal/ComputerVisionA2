3e872c9b0fefe05c750a5f45a7b0f693cbe14a4a
AWESOMESAUCE170 <55606249+AWESOMESAUCE170@users.noreply.github.com>
2021-09-27 16:32:34 -0500

    Debugging

--- detectCircles.m
+++ detectCircles.m
@@ -1,13 +1,13 @@
 function centers = detectCircles(oImage, radius)
-
-imageMat = imread(oImage);
-
+threshPctg = .9;
+imMat = imread(oImage);
+greyMat = im2gray(imMat);
 %get edges as matrix of 0's and 1's. Edges are 1's
-edgesMat = edge(imageMat);
+edgesMat = edge(greyMat);
 %get coordinates of edge points
 [pointRows, pointColumns] = find(edgesMat);
-paddedPointRows = pointRows + (radius - 1);
-paddedPointColumns = pointColumns + (radius - 1);
+paddedPointRows = pointRows + (radius);
+paddedPointColumns = pointColumns + (radius);
 relCoords = relCircleCoords(radius);
 edgeSize = size(edgesMat);
 %make padded edges matrix to accomodate circles partially out of bounds
@@ -19,8 +19,8 @@ paddedVotesMat = zeros(numPaddedVotesRows, numPaddedVotesColumns);
 for pointNum = 1:size(paddedPointRows)
     paddedPointR = paddedPointRows(pointNum);
     paddedPointC = paddedPointColumns(pointNum);
-    paddedVoteRows = relCoords(1) + paddedPointR;
-    paddedVoteColumns = relCoords(2) + paddedPointC;
+    paddedVoteRows = relCoords(:, 1) + paddedPointR;
+    paddedVoteColumns = relCoords(:, 2) + paddedPointC;
     for voteNum = 1:size(paddedVoteRows)
         paddedVoteR = paddedVoteRows(voteNum);
         paddedVoteC = paddedVoteColumns(voteNum);
@@ -30,3 +30,4 @@ end
 
 %remove the padding from the padded votes matrix.
 votes = paddedVotesMat(radius : numPaddedVotesRows - radius, radius : numPaddedVotesColumns - radius);
+imwrite(votes, 'votes.jpg');
--- relCircleCoords.m
+++ relCircleCoords.m
@@ -3,25 +3,25 @@
 function coords = relCircleCoords(radius)
 %Exluding the rightmost and leftmost column, each column in the diameter of the
 %circle will have 2 points.
-coords = zeros[2, 2*2*radius - 2];
+coords = zeros(2*2*radius - 2, 2);
 %place rightmost, leftmost, top and bottom coordinates.
 coords(1, 2) = -radius;
 coords(2, 2) = radius;
 coords(3, 1) = radius;
 coords(4, 1) = -radius;
 point = 5;
-for columnDistance = 1 : radius - 1;
+for columnDistance = 1 : radius - 1
     %for each column in between, use pythagorean theorem to find the row values
-%of the two points.
+    %of the two points.
     rowDistance = sqrt(radius^2 - columnDistance^2);
     %fill in four points based on row and column distances
-    coords(point, 1:2) = [rowDistance, columnDistance];
-    point++;
-    coords(point, 1:2) = [-rowDistance, columnDistance];
-    point++;
-    coords(point, 1:2) = [rowDistance, -columnDistance];
-    point++;
-    coords(point, 1:2) = [-rowDistance, -columnDistance];
-    point++;   
+    coords(point, 1:2) = round([rowDistance, columnDistance]);
+    point = point + 1;
+    coords(point, 1:2) = round([-rowDistance, columnDistance]);
+    point = point + 1;
+    coords(point, 1:2) = round([rowDistance, -columnDistance]);
+    point = point + 1;
+    coords(point, 1:2) = round([-rowDistance, -columnDistance]);
+    point = point + 1;  
 end
 
